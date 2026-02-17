6ea1723f7d1d6f9bced4e2368433c54653584ef2
deviarkdev-ship-it <deviarkdev@gmail.com>
2026-02-17 12:53:09 +0400

    Update mongo.py

--- Music/core/mongo.py
+++ Music/core/mongo.py
@@ -1,6 +1,6 @@
-from motor .motor_asyncio import AsyncIOMotorClient
+from motor.motor_asyncio import AsyncIOMotorClient
 from pymongo import MongoClient
-from pymongo .errors import OperationFailure ,ServerSelectionTimeoutError
+from pymongo.errors import OperationFailure ,ServerSelectionTimeoutError
 from config import MONGO_DB_URI ,MONGO_DB_NAME
 from ..logging import LOGGER
 import time
